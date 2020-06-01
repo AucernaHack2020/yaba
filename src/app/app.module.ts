aa7c583b336d5b1231bd3dd6b068c83923576ed5
GalinaLapushinskaya <galina.lapushinskaya@aucerna.com>
2020-06-01 14:40:02 -0600

    Add beer-selection component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StyleSelectorComponent } from './view/style-selector/style-selector.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
-import { StyleSelectorItemComponent } from './view/style-selector/style-selector-item/style-selector-item.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './view/login/login.component';
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StyleSelectorComponent } from "./view/style-selector/style-selector.component";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
+import { StyleSelectorItemComponent } from "./view/style-selector/style-selector-item/style-selector-item.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from "./view/login/login.component";
+import { BeerSelectionComponent } from "./view/beer-selection/beer-selection.component";
 import { IngredientsSelectionComponent } from './view/ingredients-selection/ingredients-selection.component';
-
 
 @NgModule({
   declarations: [
@@ -24,13 +24,15 @@ import { IngredientsSelectionComponent } from './view/ingredients-selection/ingr
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
     BrowserAnimationsModule,
     MatGridListModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
