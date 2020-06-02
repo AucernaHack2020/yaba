0eaae8eec403d580c3cc0a31173e2a6fa9869dc4
AlexLapushinski <Alex.Lapushinski@aucerna.com>
2020-06-02 15:21:08 -0600

    added beer-selection to ingredients-selection way

--- src/app/view/beer-selection/beer-selection.component.ts
+++ src/app/view/beer-selection/beer-selection.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Style } from 'src/app/model';
+import { Component, OnInit } from "@angular/core";
+import { Style } from "src/app/model";
+import { Router } from "@angular/router";
 
 export interface Template {
   name: string;
@@ -10,22 +11,27 @@ export interface Template {
 }
 
 @Component({
-  selector: 'yaba-beer-selection',
-  templateUrl: './beer-selection.component.html',
-  styleUrls: ['./beer-selection.component.css'],
+  selector: "yaba-beer-selection",
+  templateUrl: "./beer-selection.component.html",
+  styleUrls: ["./beer-selection.component.css"],
 })
 export class BeerSelectionComponent implements OnInit {
+  constructor(private router: Router) {}
   style: Style;
   myRecipes: Template[] = [
     {
-      name: 'Breakfast Lager',
-      type: 'lager',
-      color: 'salmon',
+      name: "Breakfast Lager",
+      type: "lager",
+      color: "salmon",
       rows: 1,
       cols: 1,
     },
-    { name: 'Relax after work', type: 'ale', color: 'green', rows: 1, cols: 2 },
+    { name: "Relax after work", type: "ale", color: "green", rows: 1, cols: 2 },
   ];
 
   ngOnInit(): void {}
+
+  btnClick() {
+    this.router.navigateByUrl("/ingredient-selection");
+  }
 }
