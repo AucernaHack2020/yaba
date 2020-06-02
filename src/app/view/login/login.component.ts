3db82e1c5a33d56d55815a050a060a847713bb26
AlexLapushinski <Alex.Lapushinski@aucerna.com>
2020-06-02 15:02:43 -0600

    login form - click button for next page

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'yaba-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  selector: "yaba-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 /**
  * @title Input with a clear button
  */
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  btnClick() {
+    //if (email === 'ok' && password === 'ok') {
+    this.router.navigateByUrl("/ingredients-selection");
+    //} else {
+    //  console.log('Invalid email or password');
+    //}
+  }
 }
