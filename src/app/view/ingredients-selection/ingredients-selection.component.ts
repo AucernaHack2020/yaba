0c26f0364087c0a3b3bfcd1a55acb846712108ae
GalinaLapushinskaya <galina.lapushinskaya@aucerna.com>
2020-06-02 15:32:45 -0600

    Add Yeast table to the page

--- src/app/view/ingredients-selection/ingredients-selection.component.ts
+++ src/app/view/ingredients-selection/ingredients-selection.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { Style, GrainIngredient, HopIngredient } from "src/app/model";
+import {
+  Style,
+  GrainIngredient,
+  HopIngredient,
+  YeastIngredient,
+} from "src/app/model";
 import { PipeCollector } from "@angular/compiler/src/template_parser/binding_parser";
-import { Router } from "@angular/router";
 
-let GRAIN_DATA: GrainIngredient[] = [
+const GRAIN_DATA: GrainIngredient[] = [
   {
     _id: "1",
     name: "Cargill Special Pale",
@@ -11,7 +15,6 @@ let GRAIN_DATA: GrainIngredient[] = [
     color: 3.5,
     percent: 87.21,
     weight: 15,
-    exclude: false,
     usage: "mash",
   },
   {
@@ -21,7 +24,6 @@ let GRAIN_DATA: GrainIngredient[] = [
     color: 3.5,
     percent: 2.91,
     weight: 15,
-    exclude: false,
     usage: "mash",
   },
 ];
@@ -40,13 +42,21 @@ const HOP_DATA: HopIngredient[] = [
   },
 ];
 
+const YEAST_DATA: YeastIngredient[] = [
+  {
+    _id: "1",
+    name: "Some Yeast",
+    attenuation: 90,
+    weight: 3.12,
+  },
+];
+
 @Component({
   selector: "yaba-ingredients-selection",
   templateUrl: "./ingredients-selection.component.html",
   styleUrls: ["./ingredients-selection.component.css"],
 })
 export class IngredientsSelectionComponent implements OnInit {
-  constructor(private router: Router) {}
   displayedGrainColumns: string[] = [
     "name",
     "extract",
@@ -54,7 +64,6 @@ export class IngredientsSelectionComponent implements OnInit {
     "percent",
     "weight",
     "usage",
-    "exclude",
   ];
   grainDataSource = GRAIN_DATA;
 
@@ -70,13 +79,36 @@ export class IngredientsSelectionComponent implements OnInit {
   ];
   hopDataSource = HOP_DATA;
 
+  displayedYeastColumns: string[] = ["name", "attenuation", "weight"];
+  yeastDataSource = YEAST_DATA;
+
   ngOnInit(): void {}
 
   addGrain() {
-    GRAIN_DATA = [...GRAIN_DATA];
+    GRAIN_DATA.push({
+      _id: "3",
+      name: "",
+      extract: 0,
+      color: 0,
+      percent: 0,
+      weight: 0,
+      usage: "mash",
+    });
+    console.log(GRAIN_DATA);
   }
 
-  btnClick() {
-    this.router.navigateByUrl("/flow-chart");
+  addHop() {
+    HOP_DATA.push({
+      _id: "",
+      name: "0",
+      alpha: 0,
+      mode: "pellet",
+      weight: 0,
+      volume: 0,
+      usage: "boil",
+      temperature: 0,
+      ibu: 0,
+    });
+    console.log(HOP_DATA);
   }
 }
--- src/app/view/style-selector/style-selector.component.ts
+++ src/app/view/style-selector/style-selector.component.ts
@@ -1,39 +1,69 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Style, Category } from 'src/app/model';
-import { DataService } from 'src/app/services/data.service';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Style, Category } from "src/app/model";
+import { DataService } from "src/app/services/data.service";
 
 @Component({
-    selector: 'yaba-style-selector',
-    templateUrl: './style-selector.component.html',
-    styleUrls: ['./style-selector.component.css']
+  selector: "yaba-style-selector",
+  templateUrl: "./style-selector.component.html",
+  styleUrls: ["./style-selector.component.css"],
 })
 export class StyleSelectorComponent implements OnInit {
+  @Output() chooseStyle = new EventEmitter<Style>();
+  styles: Style[] = [];
+  categories: Category[] = [];
+  matrix: Style[][] = [];
+  flattenMatrix: Style[] = [];
+  // tslint:disable-next-line: max-line-length
+  colours = [
+    "#e6194B",
+    "#3cb44b",
+    "#ffe119",
+    "#4363d8",
+    "#f58231",
+    "#911eb4",
+    "#42d4f4",
+    "#f032e6",
+    "#bfef45",
+    "#fabebe",
+    "#469990",
+    "#e6beff",
+    "#9A6324",
+    "#fffac8",
+    "#800000",
+    "#aaffc3",
+    "#808000",
+    "#ffd8b1",
+    "#000075",
+    "#a9a9a9",
+  ];
 
-    @Output() chooseStyle = new EventEmitter<Style>();
-    styles: Style[] = [];
-    categories: Category[] = [];
-    matrix: Style[][] = [];
-    flattenMatrix: Style[] = [];
-    // tslint:disable-next-line: max-line-length
-    colours = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabebe', '#469990', '#e6beff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9'];
+  constructor(private data: DataService) {}
 
-    constructor(
-        private data: DataService
-    ) { }
+  async ngOnInit() {
+    this.styles = await this.data.styles().toPromise();
+    this.categories = await this.data.categories().toPromise();
+    const tmp = this.categories.map((cat) =>
+      this.styles.filter((style) => style.category._id === cat._id)
+    );
+    const max = tmp
+      .map((styles) => styles.length)
+      .reduce((prev, current) => Math.max(prev, current));
+    tmp.forEach((styles, i) =>
+      styles.forEach(
+        (style) => (style.colour = this.colours[i % this.colours.length])
+      )
+    );
+    tmp.forEach((styles) =>
+      styles.unshift(...Array.from({ length: max - styles.length }, () => null))
+    );
+    this.matrix = this.categories.map(() => []);
+    tmp.forEach((styles, i) =>
+      styles.forEach((style, j) => (this.matrix[j][i] = style))
+    );
+    this.matrix.forEach((styles) => this.flattenMatrix.push(...styles));
+  }
 
-    async ngOnInit() {
-        this.styles = await this.data.styles().toPromise();
-        this.categories = await this.data.categories().toPromise();
-        const tmp = this.categories.map(cat => this.styles.filter(style => style.category._id === cat._id));
-        const max = tmp.map(styles => styles.length).reduce((prev, current) => Math.max(prev, current));
-        tmp.forEach((styles, i) => styles.forEach(style => style.colour = this.colours[i % this.colours.length]));
-        tmp.forEach(styles => styles.unshift(...Array.from({ length: max - styles.length }, () => null)));
-        this.matrix = this.categories.map(() => []);
-        tmp.forEach((styles, i) => styles.forEach((style, j) => this.matrix[j][i] = style));
-        this.matrix.forEach(styles => this.flattenMatrix.push(...styles));
-    }
-
-    choose(style: Style) {
-        this.chooseStyle.emit(style);
-    }
+  choose(style: Style) {
+    this.chooseStyle.emit(style);
+  }
 }
