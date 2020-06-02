dd805a26a71730356460180fb115708f08c0ec62
GalinaLapushinskaya <galina.lapushinskaya@aucerna.com>
2020-06-02 10:58:21 -0600

    Login page - small fix

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'yaba-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  selector: "yaba-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 /**
  * @title Input with a clear button
  */
-@Component({
-  selector: 'login.component',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-})
 export class LoginComponent implements OnInit {
   constructor() {}
 
