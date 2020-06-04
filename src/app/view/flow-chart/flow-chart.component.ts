c905b8aa3ae4aad12c00cade11a631a5a8379627
GalinaLapushinskaya <galina.lapushinskaya@aucerna.com>
2020-06-04 10:47:32 -0600

    Invite friends button - launch outlook message

--- src/app/view/flow-chart/flow-chart.component.ts
+++ src/app/view/flow-chart/flow-chart.component.ts
@@ -5,33 +5,37 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatStepper } from '@angular/material/stepper';
 
 @Component({
-    selector: 'yaba-flow-chart',
-    templateUrl: './flow-chart.component.html',
-    styleUrls: ['./flow-chart.component.css']
+  selector: 'yaba-flow-chart',
+  templateUrl: './flow-chart.component.html',
+  styleUrls: ['./flow-chart.component.css'],
 })
 export class FlowChartComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @ViewChild('stepper') stepper: MatStepper;
 
-    @Input() recipe: Recipe;
-    @ViewChild('stepper') stepper: MatStepper;
+  constructor(private calc: CalcService, private snackBar: MatSnackBar) {}
 
-    constructor(
-        private calc: CalcService,
-        private snackBar: MatSnackBar
-    ) { }
+  ngOnInit(): void {
+    //
+  }
 
-    ngOnInit(): void {
-        //
-    }
+  get og() {
+    return this.calc.og(this.recipe);
+  }
 
-    get og() {
-        return this.calc.og(this.recipe);
-    }
+  get totalWeight() {
+    return this.calc.totalWeight(this.recipe);
+  }
 
-    get totalWeight() {
-        return this.calc.totalWeight(this.recipe);
-    }
+  finishMash() {
+    this.snackBar
+      .open('Mash has been finished!', 'Next step?')
+      .onAction()
+      .subscribe(() => this.stepper.next());
+  }
 
-    finishMash() {
-        this.snackBar.open('Mash has been finished!', 'Next step?').onAction().subscribe(() => this.stepper.next());
-    }
+  inviteFriends() {
+    location.href =
+      'mailto:mybeer.buddies@shaw.ca?subject=Beer is ready&body=Bring sausages and pretzels!';
+  }
 }
